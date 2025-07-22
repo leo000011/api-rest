5ef1410c73d6c344a203db16f671a9507c3d9d7e
Leo <leandroano8@gmail.com>
2025-07-22 16:12:00 -0300

    commit

--- src/Middleware/myMiddleware.ts
+++ src/Middleware/myMiddleware.ts
@@ -1,10 +1,5 @@
 import {Request, Response, NextFunction} from 'express'
 
 export function myMiddleware1(req:Request, res:Response, next:NextFunction ) {
-    req.user_id = "1235456"
 
-
-    console.log(`Passou no teste ${req.user_id}`) 
-
-    return next()
 }
